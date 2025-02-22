c8a56683c8045b4b4282e6d41999ed96547b074c
wolxXx <devops@wolxxx.de>
2025-02-22 10:23:00 +0100

    Add build script for multi-version Docker image automation
    This script automates the build and push process for multiple PHP versions. It ensures each version is built with no cache and tagged appropriately before being pushed to the repository. The script handles errors effectively and provides informative progress output.

---
+++ build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+set -o errexit
+set -o pipefail
+
+HERE=$(dirname $(readlink -f $0));
+cd "$HERE" || exit 1;
+
+directories=("84" "83" "82" "81" "80" "74");
+
+for dir in "${directories[@]}"; do
+  cd "$HERE" || exit 1;
+  cd "$dir" || exit 1;
+
+  echo "Processing $dir..."
+
+  VERSION=$(head -n 1 "Dockerfile" | cut -d '=' -f2)
+  echo "Version found in $VERSION"
+  
+  docker build --no-cache --compress -t "wolxxxy/php$dir:$VERSION" .
+  docker push "wolxxxy/php$dir:$VERSION" "wolxxxy/php$dir:latest"
+
+  echo "$dir completed.";
+done
+
+echo "all done";
