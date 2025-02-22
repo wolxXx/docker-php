1bfeec76a9840b9b1ae65a0b222ab576572b27a6
wolxXx <devops@wolxxx.de>
2025-02-22 10:52:44 +0100

    Refactor build process to use a reusable function
    Introduced a `build` function to streamline the docker build and push steps, reducing code duplication. Updated the loop to execute builds in parallel for improved efficiency.

--- build.sh
+++ build.sh
@@ -9,21 +9,26 @@ cd "$HERE" || exit 1;
 
 directories=("84" "83" "82" "81" "80" "74");
 
+build(){
+   cd "$HERE" || exit 1;
+   cd "$1" || exit 1;
+
+   echo "Processing $1..."
+
+   VERSION=$(head -n 1 "Dockerfile" | cut -d '=' -f2)
+   echo "Version found in $VERSION"
+
+   docker build --no-cache --compress -t "wolxxxy/php$1:$VERSION" -t "wolxxxy/php$1:latest"  .
+   docker push "wolxxxy/php$1:$VERSION"
+   docker push "wolxxxy/php$1:latest"
+
+   echo "$1 completed.";
+}
+
 for dir in "${directories[@]}"; do
-  cd "$HERE" || exit 1;
-  cd "$dir" || exit 1;
-
-  echo "Processing $dir..."
-
-  VERSION=$(head -n 1 "Dockerfile" | cut -d '=' -f2)
-  echo "Version found in $VERSION"
-  
-  docker build --no-cache --compress -t "wolxxxy/php$dir:$VERSION" .
-  docker build --no-cache --compress -t "wolxxxy/php$dir:latest" .
-  docker push "wolxxxy/php$dir:$VERSION"
-  docker push "wolxxxy/php$dir:latest"
-
-  echo "$dir completed.";
+  build "$dir" &
 done
 
-echo "all done";
+exit 0;
+
+docker build --no-cache --compress -t "wolxxxy/php84:1.0" -t "wolxxxy/php84:latest" .
