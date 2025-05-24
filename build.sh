ea8771c9246f7a7a129987ff410650bc132e0ec2
wolxXx <devops@wolxXx.de>
2025-05-24 09:13:38 +0200

    Add support for PHP 8.5 in Docker build process
    Introduced a new Dockerfile for PHP 8.5 with required dependencies and configuration. Updated the build script to include the new version in the build process. This ensures compatibility with the latest PHP version.

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ set -o pipefail
 HERE=$(dirname $(readlink -f $0));
 cd "$HERE" || exit 1;
 
-directories=("84" "83" "82" "81" "80" "74" "73" "72" "71" "70" "56");
+directories=("85" "84" "83" "82" "81" "80" "74" "73" "72" "71" "70" "56");
 
 docker build --no-cache --compress -t "wolxxxy/phpbase:1.0" -t "wolxxxy/phpbase:latest" .
 
